fe008090528763908f81716ab9432bc394108206
awilby <antonellawilby@gmail.com>
2013-01-16 18:55:06 -0800

    removed unnecessary files

---
+++ state_estimator/msg_gen/lisp/_package_ControlInputs.lisp
@@ -0,0 +1,14 @@
+(cl:in-package state_estimator-msg)
+(cl:export '(HEADER-VAL
+          HEADER
+          UROLL-VAL
+          UROLL
+          UPITCH-VAL
+          UPITCH
+          UYAW-VAL
+          UYAW
+          UDEPTH-VAL
+          UDEPTH
+          USURGE-VAL
+          USURGE
+))
